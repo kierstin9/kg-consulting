696e4825822c28527afa30a3364486a634b0e1bf
kierstin9 <kierstin.gray@gmail.com>
2021-03-17 13:25:51 -0400

    Add files via upload
    upload of first build update / added js and contact form; some edits to layout and css for @media breakpoints

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var JFL_210694196236157 = new JotformFeedback({
+          formId: '210694196236157',
+          base: 'https://form.jotform.com/',
+          windowTitle: 'Kierstin Gray Consulting Contact Form',
+          background: 'rgba(175,39,24,0.5)',
+          fontColor: '#595959',
+          type: '2',
+          height: 500,
+          width: 700,
+          openOnLoad: false
+        });
+       
